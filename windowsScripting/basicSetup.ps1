a6538f763a3723f9817b60f3e92f20444402ce6f
natalie363 <natalie.eckles@mymail.champlain.edu>
2023-03-27 20:10:45 -0400

    Added networking to basic setup script

--- windowsScripting/basicSetup.ps1
+++ windowsScripting/basicSetup.ps1
@@ -8,4 +8,20 @@ $username = Read-Host
 echo "Please enter a password for the user '$username': "
 $password = Read-Host -AsSecureString
 New-LocalUser -Name $username -Password $password
-Add-LocalGroupMember -Group "Administrators" -Member $username 
+Add-LocalGroupMember -Group "Administrators" -Member $username
+
+# Networking! Prompts user for each component
+echo "Please enter the host IP address: "
+$hostIP = Read-Host
+echo "Please enter the prefix length (ex. 24): "
+$prefixLength = Read-Host 
+echo "Please enter the default gateway: "
+$defaultGateway = Read-Host
+echo "Please enter the DNS server address: "
+$DNSAddress = Read-Host
+Get-NetAdapter | Select-Object InterfaceIndex
+echo "Please enter the InterfaceIndex number displayed on the screen: "
+$intID = Read-Host
+# Sets IP and netmask using user inputs
+Set-NetIPAddress -InterfaceIndex $intID -IPAddress $hostIP -PrefixLength $prefixLength -DefaultGateway
+Set-DNSClientServerAddress -InterfaceIndex $intID -ServerAddresses ("\$DNSAddress")
