c35b5b0a1df86ebc642316db8594f6f88c0d79da
natalie363 <natalie.eckles@mymail.champlain.edu>
2023-03-27 21:41:04 -0400

    Added capability to change hostname

--- windowsScripting/basicSetup.ps1
+++ windowsScripting/basicSetup.ps1
@@ -1,10 +1,12 @@
 # This script is intended for use before joining a domain
 # It sets up networking, makes a local user, and changes the hostname
 
+
 # Conditional to see if you would like to make a new user
 echo "Would you like to make a local admin user account? (y or n): "
 $newUser = Read-Host
 if($newUser -eq 'y') {
+
 # Makes a local user, requesting a username and password
 echo "Please enter a username for a new local user: "
 $username = Read-Host
@@ -13,10 +15,13 @@ $password = Read-Host -AsSecureString
 New-LocalUser -Name $username -Password $password
 Add-LocalGroupMember -Group "Administrators" -Member $username
 }
+
+
 # check if the user wants to set up networking
 echo "Would you like to set up networking? (y or n): "
 $networking = Read-Host
 if ($networking -eq 'y') {
+
 # Networking! Prompts user for each component
 echo "Please enter the host IP address: "
 $hostIP = Read-Host
@@ -30,13 +35,29 @@ $displayIntID = Get-NetAdapter | Select-Object InterfaceIndex
 Write-Host $displayIntID
 echo "Please enter the InterfaceIndex number displayed on the screen: "
 $intID = Read-Host
+
 # Removes existing IP configurations to prevent errors
 Remove-NetIPAddress -InterfaceIndex $intID -Confirm:$false
 # This removes the default gateway, but will throw an error if there are no routes set, so I have set it to 
 # ignore errors
 Remove-NetRoute -InterfaceIndex $intID -confirm:$false -erroraction 'silentlycontinue'
 Set-DNSClientServerAddress -InterfaceIndex $intID -ResetServerAddresses
+
 # Sets IP and netmask using user inputs
 New-NetIPAddress -InterfaceIndex $intID -IPAddress $hostIP -PrefixLength $prefixLength -DefaultGateway $defaultGateway
 Set-DNSClientServerAddress -InterfaceIndex $intID -ServerAddresses ("$DNSAddress")
+}
+
+
+# Conditional to see if you would like to change the hostname
+echo "Would you like to change the hostname? (y or n): "
+$changeName = Read-Host
+if($changeName = 'y') {
+# Collects required variables
+echo "Enter the new hostname: "
+$newHostname = Read-Host
+echo "Enter a LOCAL username with permissions to change the hostname: "
+$userAccount = Read-Host
+rename-computer -NewName $newHostname -force -LocalCredential $userAccount
+echo "Please restart your computer for this change to take effect."
 }
