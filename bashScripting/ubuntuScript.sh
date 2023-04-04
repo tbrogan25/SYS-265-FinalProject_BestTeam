cc94449251f88b30b0b55f5ac924b993411c6442
natalie363 <natalie.eckles@mymail.champlain.edu>
2023-04-04 02:54:47 +0000

    networking portion of script works

--- bashScripting/ubuntuScript.sh
+++ bashScripting/ubuntuScript.sh
@@ -29,3 +29,47 @@ then
 	#sed -i "1i ${hostname}" hostname
 	echo "Please restart your system to allow the change to take affect."
 fi
+
+# Setting up Networking
+read -p "Would you like to set up networking? (y or n): " changeNetworking
+if [ $changeNetworking = y ];
+then
+	read -p " Would you like to use DHCP? (y or n): " dhcp
+	if [ $dhcp = y ];
+	then
+		rm /etc/netplan/00-installer-config.yaml
+		touch /etc/netplan/00-installer-config.yaml
+		filepath=/etc/netplan/00-installer-config.yaml
+		ens=$(ls /sys/class/net | grep ens)
+		echo "network:" >> $filepath
+		echo "  version: 2" >> $filepath
+		echo "  renderer: networkd" >> $filepath
+		echo "  ethernets:" >> $filepath
+		echo "    $ens:" >> $filepath
+		echo "      dhcp4: true" >> $filepath
+		netplan apply
+	else
+		read -p "Enter the IP address with CIDR notation: " ipAddress
+		read -p "Enter the default gateway address: " gateway
+		read -p "Enter the DNS search domain: " domain
+		read -p "Enter the DNS server addresss: " dns
+		rm /etc/netplan/00-installer-config.yaml
+		touch /etc/netplan/00-installer-config.yaml
+		filepath=/etc/netplan/00-installer-config.yaml
+		ens=$(ls /sys/class/net | grep ens)
+		echo "network:" >> $filepath
+		echo "  version: 2" >> $filepath
+		echo "  ethernets:" >> $filepath
+		echo "    $ens:" >> $filepath
+		echo "      addresses:" >> $filepath
+		echo "      - $ipAddress" >> $filepath
+		echo "      gateway4: $gateway" >> $filepath
+		echo "      nameservers:" >> $filepath
+		echo "        search: [$domain]" >> $filepath
+		echo "        addresses:" >> $filepath
+		echo "        - $dns" >> $filepath
+		echo "      dhcp4: false" >> $filepath
+		netplan apply
+
+	fi
+fi
